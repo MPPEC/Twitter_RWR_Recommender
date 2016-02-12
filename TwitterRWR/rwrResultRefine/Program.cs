bebbec9bd9ef154f4799f71f6d4623144dda543f
Brian, Song <dilettantest@gmail.com>
2016-02-12 17:54:35 +0900

    Result Refine Added

---
+++ TwitterRWR/rwrResultRefine/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rwrResultRefine
+{
+    class Program
+    {
+        // Command Line Argument: C:\Users\dilet\Desktop\TwitterDB C:\Users\dilet\Desktop\Twitter_RWR_Result 
+        static void Main(string[] args)
+        {
+            string dbDirPath = args[0];
+            string resutlDirPath = args[1];
+            string[] dbCollection = Directory.GetFiles(dbDirPath, "*.sqlite");
+            string[] resultCollection = Directory.GetFiles(resutlDirPath, "*.txt");
+            SortedDictionary<long, RWRMetric> rwrResultList = new SortedDictionary<long, RWRMetric>();
+            foreach (string dbPath in dbCollection)
+            {
+                long egoID = long.Parse(Path.GetFileNameWithoutExtension(dbPath));
+                rwrResultList.Add(egoID, null);
+            }
+
+            foreach (string resultFilePath in resultCollection)
+            {
+                // Get Result from result file
+                if (File.Exists(resultFilePath))
+                {
+                    long egoID;
+                    int method, kFold, iteration, like, hit, friend;
+                    string executionTime;
+                    double MAP, recall;
+
+                    using (StreamReader reader = new StreamReader(resultFilePath))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            string[] tokens = line.Split('\t');
+                            egoID = long.Parse(tokens[0]);
+                            method = int.Parse(tokens[1]);
+                            kFold = int.Parse(tokens[2]);
+                            iteration = int.Parse(tokens[3]);
+                            MAP = double.Parse(tokens[4]);
+                            recall = double.Parse(tokens[5]);
+                            like = int.Parse(tokens[6]);
+                            hit = int.Parse(tokens[7]);
+                            friend = int.Parse(tokens[8]);
+                            executionTime = tokens[9];
+                            if (rwrResultList.ContainsKey(egoID))
+                                rwrResultList[egoID] =
+                                    new RWRMetric(egoID, method, kFold, iteration, MAP, recall, like, hit, friend, executionTime);
+                        }
+                    }
+                }
+
+                // Replace File Result (In increasing ego ID order)
+                File.Delete(resultFilePath);
+                using (StreamWriter writer = new StreamWriter(resultFilePath, true))
+                {
+                    foreach (long egoID in rwrResultList.Keys)
+                    {
+                        if (rwrResultList[egoID] != null)
+                            rwrResultList[egoID].logResultIntoFile(writer);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TwitterRWR/rwrResultRefine/RWRMetric.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rwrResultRefine
+{
+    class RWRMetric
+    {
+        // Properties
+        public long egoID;
+        private int method;
+        private int kFold;
+        private int iteration;
+        private double MAP;
+        private double recall;
+        private int like;
+        private int hit;
+        private int friend;
+        private string executionTime;
+
+        // Constructor
+        public RWRMetric
+        (long egoID, int method, int kFold, int iteration, double MAP, double recall, int like, int hit, int friend, string executionTime)
+        {
+            this.egoID = egoID;
+            this.method = method;
+            this.kFold = kFold;
+            this.iteration = iteration;
+            this.MAP = MAP;
+            this.recall = recall;
+            this.like = like;
+            this.hit = hit;
+            this.friend = friend;
+            this.executionTime = executionTime;
+        }
+
+        // Output RWR Result
+        public void logResultIntoFile(StreamWriter writer)
+        {
+            writer.WriteLine("{0}\t{1}\t{2}\t{3}\t{4:F15}\t{5:F15}\t{6}\t{7}\t{8}\t{9}",
+            egoID, method, kFold, iteration, MAP, recall, like, hit, friend, executionTime);
+            writer.Flush();
+        }
+    }
+}
